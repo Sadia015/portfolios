e61c79823ccdc076f5b5f0e72d4cc429f4219389
Sadia015 <147911614+Sadia015@users.noreply.github.com>
2024-02-18 08:04:37 -0800

    Add files via upload

---
+++ javascript table assigment/app.js
@@ -0,0 +1,38 @@
+var userInput= +prompt('enter number')
+for (var i=1; i<=10; i++){
+document.write(userInput+'X'+i+'='+userInput*i,'<br>')}
+
+var userInput= +prompt('enter number')
+for (var i=1; i<=10; i++){
+document.write(userInput+'X'+i+'='+userInput*i,'<br>')}
+
+var userInput= +prompt('enter number')
+for (var i=1; i<=10; i++){
+document.write(userInput+'X'+i+'='+userInput*i,'<br>')}
+
+var userInput= +prompt('enter number')
+for (var i=1; i<=10; i++){
+document.write(userInput+'X'+i+'='+userInput*i,'<br>')}
+
+var userInput= +prompt('enter number')
+for (var i=1; i<=10; i++){
+document.write(userInput+'X'+i+'='+userInput*i,'<br>')}
+
+var userInput= +prompt('enter number')
+for (var i=1; i<=10; i++){
+document.write(userInput+'X'+i+'='+userInput*i,'<br>')}
+
+var userInput= +prompt('enter number')
+for (var i=1; i<=10; i++){
+document.write(userInput+'X'+i+'='+userInput*i,'<br>')}
+
+var userInput= +prompt('enter number')
+for (var i=1; i<=10; i++){
+document.write(userInput+'X'+i+'='+userInput*i,'<br>')}
+
+var userInput= +prompt('enter number')
+for (var i=1; i<=10; i++){
+document.write(userInput+'X'+i+'='+userInput*i,'<br>')}
+
+
+
